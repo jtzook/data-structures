8355f4bfd5ae611da59922c1a545785d9a2fcc2e
Jared Zook <jaredzook@gmail.com>
2021-03-21 13:16:06 -0700

    remove dups

--- src/linked-lists/helpers.test.ts
+++ src/linked-lists/helpers.test.ts
@@ -1,5 +1,29 @@
-describe('removeDups', () => {
-  const inputList = ['maui', 'Maui', 'callie', 'callie', 'otto', 'luna', 'luna']
+import { removeDups } from './helpers'
+import LinkedList from './linked-list'
 
-  it('removes duplicates from an unsorted singly-linked list', () => {})
+describe('removeDups', () => {
+  const inputList = [
+    'luna',
+    'maui',
+    'Maui',
+    'callie',
+    'callie',
+    'otto',
+    'luna',
+    'luna'
+  ]
+
+  it('removes duplicates from an unsorted singly-linked list', () => {
+    const ll = LinkedList.fromArray(inputList)
+
+    removeDups(ll)
+
+    expect(LinkedList.toArray(ll)).toEqual([
+      'luna',
+      'maui',
+      'Maui',
+      'callie',
+      'otto'
+    ])
+  })
 })
--- src/linked-lists/helpers.ts
+++ src/linked-lists/helpers.ts
@@ -1,4 +1,22 @@
 import LinkedList from './linked-list'
 
 // remove duplicates from a singly-linked list
-const removeDups = (ll: LinkedList) => {}
+export const removeDups = (ll: LinkedList) => {
+  let prev = ll.head
+
+  const seen = new Set([prev.data])
+
+  while (prev.next) {
+    let cur = prev.next
+
+    if (seen.has(cur.data)) {
+      prev.next = cur.next
+      cur = null
+    } else {
+      seen.add(cur.data)
+      prev = cur
+    }
+  }
+
+  console.log('cur', LinkedList.toArray(ll))
+}
