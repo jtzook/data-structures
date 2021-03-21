c9c270faaf13f87bd8d973e6ba5f44ecd0abffc6
Jared Zook <jaredzook@gmail.com>
2021-03-21 12:01:20 -0700

    toArray method

--- src/linked-lists/linked-list.test.ts
+++ src/linked-lists/linked-list.test.ts
@@ -1,17 +1,42 @@
 import LinkedList from './linked-list'
 
 describe('linked-list', () => {
-  const data = 'll data'
+  let ll
+  const initData = 'll data'
+  const extraData = 'magic'
+  const inputList = [4, 3, 2, 1, 0]
+
+  beforeEach(() => {
+    ll = new LinkedList(initData)
+  })
 
   it('instantiates a linked list with one node', () => {
-    const ll = new LinkedList(data)
-
-    expect(ll.head.data).toEqual(data)
+    expect(ll.head.data).toEqual(initData)
     expect(ll.head.next).toBeNull()
   }),
     it('inserts an element into a singly linked list', () => {
-      const ll = new LinkedList(data)
+      ll.insert(extraData)
+      ll.insert(extraData)
 
-      ll.insert('magic')
+      expect(ll.head.next.data).toEqual(extraData)
+    }),
+    it('populates a linked list from array input', () => {
+      ll.remove(initData)
+
+      inputList.forEach((data) => ll.insert(data))
+
+      console.log('arr', LinkedList.toArray(ll))
+    }),
+    it('returns an empty head node after removing only data node', () => {
+      ll.remove(initData)
+
+      expect(ll.head).toBeNull()
+    }),
+    it('removes an item from a singly linked list', () => {
+      ll.insert(extraData)
+
+      ll.remove(extraData)
+
+      expect(ll.head.next).toBeNull()
     })
 })
--- src/linked-lists/linked-list.ts
+++ src/linked-lists/linked-list.ts
@@ -9,8 +9,61 @@ class LinkedList {
     this.head = new LLNode(data)
   }
 
-  insert(data: any) {
-    this.head.next = new LLNode(data)
+  public insert(data: any) {
+    const node = new LLNode(data)
+
+    if (!this.head) {
+      this.head = node
+    } else if (!this.head.next) {
+      this.head.next = node
+    } else {
+      // find last node
+
+      let cur = this.head.next
+
+      while (cur?.next) {
+        cur = cur.next
+      }
+
+      cur.next = node
+    }
+  }
+
+  public remove(data: any) {
+    if (this.head.data === data) {
+      // data is in first node
+
+      this.head = this.head.next
+    } else if (this.head.next) {
+      // data is in later nodes
+
+      let prev = this.head
+      let cur = this.head.next
+
+      while (cur && cur.data !== data) {
+        prev = cur
+        cur = cur.next
+      }
+
+      if (cur?.data === data) {
+        prev.next = cur.next
+        cur = null
+      }
+    }
+  }
+
+  public static toArray(ll: LinkedList = null) {
+    const elements = []
+
+    let cur = ll.head
+
+    while (cur) {
+      elements.push(cur.data)
+
+      cur = cur.next
+    }
+
+    return elements
   }
 }
 
